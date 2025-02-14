7dc043d93af1381a0bea150833531fd6cc8722b8
Noman <noman@localhost.localdomain>
2025-02-14 19:19:31 +0500

    created a basic express server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,16 @@
-console.log("Initial Express App");
+import { createServer } from "node:http";
+import { Request, Response } from "express";
+import app from "./app.js";
+
+app.get("/", (_req: Request, res: Response) => {
+  res.status(200).send("<h1>Hello From Express Server</h1>");
+});
+
+(async () => {
+  const PORT = process.env.PORT ?? 4000;
+  const server = createServer(app);
+
+  server.listen(PORT, () => {
+    console.log(`listening at: http://127.0.0.01:${PORT}`);
+  });
+})();
